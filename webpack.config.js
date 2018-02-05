6149c49c9767895a252da68b2dd6c7ead8f9c400
Janko Ahlbrandt <keyj@keyj.de>
2018-02-05 18:02:42 +0100

    Bugfix: Missing module config for Webpack.

--- webpack.config.js
+++ webpack.config.js
@@ -15,7 +15,8 @@ module.exports = {
     output: {
         path: path.join(__dirname, "build"),
         filename: "[name].js",
-        publicPath: "/js"
+        libraryTarget: "umd",
+        library: "WSH"
     },
     module: {
         rules: [
