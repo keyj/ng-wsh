332149983a50891edbee49528c09e61d3c8a5ded
Janko Ahlbrandt <ahlbrandj@g230-mb01.local>
2018-02-08 10:12:15 +0100

    Bugfix: finally a working webpack config. Included: Babel-preset-env (still version 6 of Babel) Plugins: * transform-runtime * transform-decorators-legacy Found in angular 2 example webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ module.exports = {
         ],
         dist: [
             // we include only "terminal.js" as it is the main object:
-            path.join(__dirname, "wsh.js.es5", "lib", "terminal.js")
+            path.join(__dirname, "wsh.js", "lib", "terminal.js")
         ],
     },
     output: {
@@ -28,6 +28,7 @@ module.exports = {
               loader: 'babel-loader',
               options: {
                 presets: ['babel-preset-env'],
+                plugins: ['transform-runtime', 'transform-decorators-legacy']
                 // plugins: [require('@babel/plugin-proposal-object-rest-spread')]
               }
             }
